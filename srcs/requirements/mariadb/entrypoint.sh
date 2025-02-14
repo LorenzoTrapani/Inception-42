7c9ab4fdfdcd57688a9e5ac0c679497eb7acf1d9
Lorenzo <lorenzotrapani00@gmail.com>
2025-02-14 17:54:28 +0100

    nginx exit code 1

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -14,12 +14,27 @@ fi
 
 # Esegui il comando SQL per creare il database e l'utente
 echo "Configurazione iniziale del database..."
+
+# Avvia MySQL in background
+mysqld --skip-networking &
+MYSQL_PID=$!
+
+# Attendi che MySQL sia pronto
+until mysqladmin ping --silent; do
+    echo "Aspettando che MySQL sia pronto..."
+    sleep 1
+done
+
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
+
+# Termina MySQL
+kill "$MYSQL_PID"
+wait "$MYSQL_PID"
 
 echo "Database e utente creati con successo!"
 
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-# until mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
-#   echo "Aspettando MariaDB..."
-#   sleep 2
-# done
+echo "inizio wp"
+
+
+cp /tmp/wp-config.php /var/www/wp-config.php
 
 # Crea il wp-config.php se non esiste
-if [ ! -f /var/www/html/wp-config.php ]; then
+if [ ! -f /var/www/wp-config.php ]; then
   curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   ./wp-cli.phar core download --allow-root
-  ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
-  ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+  ./wp-cli.phar config create \
+    --dbname="$DB_NAME" \
+    --dbuser="$DB_USER" \
+    --dbpass="$DB_PASSWORD" \
+    --dbhost="$DB_HOST" \
+    --allow-root
+  
+  # Aggiunge automaticamente le chiavi segrete di WordPress
+  wp_secret_keys=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+  echo "$wp_secret_keys" >> /var/www/html/wp-config.php
+
+  # Installa WordPress
+  #./wp-cli.phar core install \
+  #  --url=localhost \
+  #  --title=inception \
+  #  --admin_user=admin \
+  #  --admin_password=admin \
+  #  --admin_email=admin@admin.com \
+  #  --allow-root
 fi
+
+echo "wp installato"
 
 # Avvia PHP-FPM in modalità foreground
 php-fpm8.2 -F
